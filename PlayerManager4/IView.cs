e9b162a525f499d1cd02028717abe3a68cc36990
Gonçalo Sampaio <goncalo.fsampaio@gmail.com>
2024-05-15 12:27:22 +0100

    Changed project to a MVC design

---
+++ PlayerManager4/IView.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PlayerManager4
+{
+    public interface IView
+    {
+        int MainMenu();
+
+        void EndMessage();
+
+        void InvalidOption();
+
+        void AfterMenu();
+
+        Player InsertPlayer();
+
+        void ListPlayers(IEnumerable<Player> playersToList)();
+
+        int AskForMinimumScore();
+
+        PlayerOrder AskForPlayerOrder();
+    }
+}
--- PlayerManager4/Program.cs
+++ PlayerManager4/Program.cs
@@ -3,17 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace 
+namespace PlayerManager4
 {
     public class Program
     {
-        private static void Main Main() {
+        private static void Main() {
             // Initialize the player list with two players using collection
             // initialization syntax
             List<Player> playerList = new List<Player>() {
                 new Player("Best player ever", 100),
                 new Player("An even better player", 500)
             };
+
+            Controller controller = new Controller(playerList);
+
+            IVew view = new UglyView(controller, playerList);
+
+            controller.Start(view);
         }
     }
 }
